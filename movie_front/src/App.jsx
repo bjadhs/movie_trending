e46d301118f085e06bb9c12f157777a418d293ed
Bijaya Adhikari <bjadhs@gmail.com>
2025-01-25 13:18:20 +1100

    initial commit setup express and vite react and tailwind

---
+++ movie_front/src/App.jsx
@@ -0,0 +1,11 @@
+
+const App = () => {
+  return (
+    <div className='flex flex-col justify-center items-center h-full'>
+    <h1 className='text-7xl text-sky-500 underline'>App</h1>
+    App
+    </div>
+  )
+}
+
+export default App
