ede9f25b93a8c3e974dc8ba8033a517d397c1281
Bijaya Adhikari <bjadhs@gmail.com>
2025-01-27 21:31:12 +1100

    testing appwrite database connection

--- movie_front/vite-env.d.ts
+++ movie_front/vite-env.d.ts
@@ -1,11 +1,14 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
+  env: {
   readonly VITE_TMDB_API_URL: string;
   readonly VITE_TMDB_API_KEY: string;
   readonly VITE_APPWRITE_PROJECT_ID: string;
   readonly VITE_APPWRITE_DATABASE_ID: string;
   readonly VITE_APPWRITE_COLLECTION_ID: string;
+  }
+  
 }
 
 interface ImportMeta {
